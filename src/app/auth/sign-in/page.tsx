a37d12f0b8bf27c66ad834d5e189b0f4038dd809
RUDRANSHFLY <mayamayavir@gmail.com>
2025-05-24 11:22:30 +0530

    prisma bug fix

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -2,6 +2,7 @@ import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const SingIn = () => {
@@ -11,13 +12,13 @@ const SingIn = () => {
         <div className="w-full h-full bg-[url('/bg.jpg')] bg-cover flex justify-center items-center">
           <div className="flex flex-col gap-y-5 bg-slate-200 w-80 py-6 px-4 rounded-md">
             <Image
-                         src={"/logo.png"}
-                         alt="logo"
-                         width={100}
-                         height={100}
-                         className="mx-auto mt-2 rounded-md border-2 border-stone-300 p-2"
-                       />
-                       <h1 className="text-center text-2xl font-bold">Sign In</h1>
+              src={"/logo.png"}
+              alt="logo"
+              width={100}
+              height={100}
+              className="mx-auto mt-2 rounded-md border-2 border-stone-300 p-2"
+            />
+            <h1 className="text-center text-2xl font-bold">Sign In</h1>
             <Input
               name="email"
               type="email"
@@ -35,7 +36,10 @@ const SingIn = () => {
             <div className="flex flex-row items-center gap-x-2">
               <span className="text-xs">dont have an account</span>
 
-              <Link href={"/auth/sign-up"} className="text-xs text-blue-500 underline">
+              <Link
+                href={"/auth/sign-up"}
+                className="text-xs text-blue-500 underline"
+              >
                 Register it
               </Link>
             </div>
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,3 +1,4 @@
-import { PrismaClient } from "../generated/prisma"
+import { PrismaClient } from "@prisma-db-1/client";
+
 
 export const prisma = new PrismaClient();
